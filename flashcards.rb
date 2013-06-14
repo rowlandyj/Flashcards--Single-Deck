b07bea2e8f098f7af5c1a256335dbd121e90c7ef
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-14 12:01:22 -0700

    Got all the files

--- flashcards.rb
+++ flashcards.rb
@@ -8,36 +8,3 @@ class FlashCard
 
 end
 
-class FlashCardController
-  attr_reader :cards
-
-  def initialize(filename)
-    @cards = []
-    @filename = filename
-  end
-
-  def load
-    file = File.open(@filename,"r")
-    data = file.read
-    file.close
-
-    data = data.split("\n")
-    data.delete(" ")
-
-    data.each do |string|
-      @cards << string
-    end
-  end
-
-  def to_s
-    @cards.each do |line|
-      puts line
-    end
-  end
-end
-
-card = FlashCardController.new("flashcard_samples.txt")
-
-card.load
-
-puts card
