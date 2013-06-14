bcbabe3eae0c26d9ee180db9b63589fed461e2a6
dbc08 <apprentice@dbc08.local>
2013-06-14 12:15:14 -0700

    Fixed Code positions

--- flashcards.rb
+++ flashcards.rb
@@ -7,36 +7,3 @@ class FlashCard
   end
 
 end
-
-class FlashCardController
-  attr_reader :cards
-
-  def initialize(filename)
-    @cards = []
-    @filename = filename
-  end
-
-  def load
-    file = File.open(@filename,"r")
-    data = file.read
-    file.close
-
-    data = data.split("\n")
-    data.delete(" ")
-
-    data.each_slice(2)
-    
-  end
-
-  def to_s
-    @cards.each do |line|
-      puts line
-    end
-  end
-end
-
-card = FlashCardController.new("flashcard_samples.txt")
-
-card.load
-
-puts card
